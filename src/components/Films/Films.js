a7f88213893b994dd1af6beecd65957389d3e2a2
Carlos de la Cruz <104373716+carlos94cr@users.noreply.github.com>
2022-07-08 22:18:46 +0200

    carlos changes

--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -4,70 +4,82 @@ import { connect } from "react-redux";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { deleteFilms } from "../../redux/films/films.actions";
+import "./Films.scss";
 
 const Films = () => {
-  const { film } = useSelector(state => state.film);
-  const user = useSelector(state => state.auth.user);
+  const { film } = useSelector((state) => state.film);
+  const user = useSelector((state) => state.auth.user);
   //console.log(film);
   const dispatch = useDispatch();
   return (
     <>
-      <h3>Peliculas</h3>
+      <h2 className="SectionTitle">PELÍCULAS</h2>
       {film.length > 0 &&
         film.map((fil, index) => {
           return (
-            <div key={index}>
-              <p>
-                <strong>Titulo </strong>
-                {fil.name}
-              </p>
-              <img src={fil.poster} alt="film" />
-              <p>
-                <strong>Synopsis </strong>
-                {fil.synopsis}
-              </p>
-              <p>
-                <strong>Calificación </strong>
-                {fil.rated}
-              </p>
-              <p>
-                <strong>Duracion</strong>
-                {fil.duration}
-              </p>
-              <p>
-                <strong>Genero</strong>
-                {fil.genre}
-              </p>
-              <p>
-                <strong>Director</strong>
-                {fil.director}
-              </p>
-              <p>
-                <strong>Actors</strong>
-                {fil.actors}
-              </p>
-              <p>
-                <strong>Fecha estreno</strong>
-                {fil.iniDate}
-              </p>
-              <p>
-                <strong>Fecha fuera de cartelera</strong>
-                {fil.finDate}
-              </p>
-              <div>
-                {fil.isActive === "true" && <p>Disponible en cartelera</p>}
-                {fil.isActive === "false" && <p>No disponible en cartelera</p>}
+            <div className="FilmCard" key={index}>
+              <div className="FilmCard-Left">
+                <img className="Film-img" src={fil.poster} alt="film" />
               </div>
-              {user && <button
-                className="button"
-                onClick={() => {
-                  dispatch(deleteFilms(fil));
-                }}
-              >
-                Eliminar
-              </button>}
-              {user && <Link to={`/editFilms/${index}`}><button>Modificar</button></Link>}
-              {/* {user && <button
+              <div className="FilmCard-Right">
+                <div className="Films-div-long">
+                  <span className="Films-span">Título</span>
+                  <p>{fil.name}</p>
+                </div>
+                <div className="Films-div-long">
+                  <span className="Films-span">Sinopsis</span>
+                  <p>{fil.synopsis}</p>
+                </div>
+                <div className="Films-div">
+                  <span className="Films-span">Calificación</span>
+                  <p>{fil.rated}</p>
+                </div>
+                <div className="Films-div">
+                  <span className="Films-span">Duración</span>
+                  <p>{fil.duration}min.</p>
+                </div>
+                <div className="Films-div">
+                  <span className="Films-span">Género</span>
+                  <p>{fil.genre}</p>
+                </div>
+                <div className="Films-div">
+                  <span className="Films-span">Director</span>
+                  <p>{fil.director}</p>
+                </div>
+                <div className="Films-div-long">
+                  <span className="Films-span">Actores</span>
+                  <p>{fil.actors}</p>
+                </div>
+                <div className="Films-div">
+                  <span className="Films-span">Fecha estreno</span>
+                  <p>{fil.iniDate}</p>
+                </div>
+                <div className="Films-div">
+                  <span className="Films-span">Fecha fuera de cartelera</span>
+                  <p>{fil.finDate}</p>
+                </div>
+                <div>
+                  {fil.isActive === "true" && <p>Disponible en cartelera</p>}
+                  {fil.isActive === "false" && (
+                    <p>No disponible en cartelera</p>
+                  )}
+                </div>
+                {user && (
+                  <button
+                    className="button"
+                    onClick={() => {
+                      dispatch(deleteFilms(fil));
+                    }}
+                  >
+                    Eliminar
+                  </button>
+                )}
+                {user && (
+                  <Link to={`/editFilms/${index}`}>
+                    <button>Modificar</button>
+                  </Link>
+                )}
+                {/* {user && <button
                 className="button"
                 onClick={() => {
                   dispatch(editFilms(fil));
@@ -75,11 +87,11 @@ const Films = () => {
               >
                Editar
               </button>} */}
-
+              </div>
             </div>
           );
         })}
     </>
   );
 };
-export default connect(state => ({ user: state.auth.user }))(Films);
+export default connect((state) => ({ user: state.auth.user }))(Films);
